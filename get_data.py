c059b4b99ddedb24a03cf44a25242bf40beff760
Dmitri Prigojev <dmitripr@gmail.com>
2020-03-20 15:45:12 -0400

    Add files via upload
    Initial upload

---
+++ get_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import requests
+import io
+
+URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+s = requests.get(URL).content
+raw_data = pd.read_csv(io.StringIO(s.decode('utf-8')))
+raw_data.to_csv('~/raw_data_latest.csv', index=False)
