757839557bdbc19e63bea4c7aa0b81dc1c43d2eb
Dmitri Prigojev <dmitripr@gmail.com>
2020-03-23 23:10:43 -0400

    Update to use new data files

--- get_data.py
+++ get_data.py
@@ -2,12 +2,12 @@ import pandas as pd
 import requests
 import io
 
-URL_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+URL_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 s_confirmed = requests.get(URL_confirmed).content
 raw_data_confirmed = pd.read_csv(io.StringIO(s_confirmed.decode('utf-8')))
 raw_data_confirmed.to_csv('covid19_temp/raw_data_confirmed_latest.csv', index=False)
 
-URL_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+URL_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 s_deaths = requests.get(URL_deaths).content
 raw_data_deaths = pd.read_csv(io.StringIO(s_deaths.decode('utf-8')))
 raw_data_deaths.to_csv('covid19_temp/raw_data_deaths_latest.csv', index=False)
